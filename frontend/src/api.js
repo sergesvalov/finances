1a55fbf3a4a489258011751e48419ce786bec57f
Serge Svalov <wndrsw@gmail.com>
2026-03-26 10:27:59 +0200

    fix

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api', // For local dev, in production usually just /api
+  baseURL: 'http://localhost:7051/api', // For local dev, in production usually just /api
   headers: {
     'Content-Type': 'application/json',
   },
